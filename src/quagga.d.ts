3fcb786e7259c644cc6adaf845e335c484249bb8
QXDgithub <167010708+QXDgithub@users.noreply.github.com>
2024-07-12 00:39:59 +0530

    Update quagga.d.ts

--- src/quagga.d.ts
+++ src/quagga.d.ts
@@ -1,1 +1,32 @@
-
+declare module 'quagga' {
+    interface QuaggaJSResultObject {
+      codeResult: {
+        code: string;
+      };
+    }
+  
+    interface QuaggaJSConfiguration {
+      inputStream: {
+        type: string;
+        constraints: {
+          width: number;
+          height: number;
+          facingMode: string;
+        };
+        target: HTMLElement | null;
+      };
+      decoder: {
+        readers: string[];
+      };
+    }
+  
+    interface Quagga {
+      init: (config: QuaggaJSConfiguration, callback: (err: Error | null) => void) => void;
+      start: () => void;
+      stop: () => void;
+      onDetected: (callback: (data: QuaggaJSResultObject) => void) => void;
+    }
+  
+    const Quagga: Quagga;
+    export default Quagga;
+  }
